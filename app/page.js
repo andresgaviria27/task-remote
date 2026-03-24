6c70acdc5008317d6e6f8d780f43bff0117fed77
andresgaviria27 <andres_felipe_gaviria28@hotmail.com>
2026-03-23 19:08:43 -0500

    feat: init app/page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-950 text-white">
+      <h1 className="text-5xl font-bold mb-4">TaskRemote</h1>
+      <p className="text-gray-400 text-xl">Aplicación de gestión de tareas para equipos remotos, que facilita la organización y seguimiento de proyectos. Permite a los equipos trabajar de manera eficiente y colaborativa, independientemente de su ubicación geográfica.</p>
+      <div className="mt-8 px-6 py-3 bg-white text-black rounded-lg font-semibold">
+        Coming Soon
+      </div>
+    </main>
+  )
+}
